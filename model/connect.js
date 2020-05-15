f912af96a98c30526e5fd1d062eda51b7460f659
kob01 <201358775@qq.com>
2020-05-15 15:48:24 +0800

    fix

--- model/connect.js
+++ model/connect.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 // 导入config模块
 const config = require('config');
-console.log(config.get('db.host'))
+//console.log(config.get('db.host'))
+mongoose.set('useCreateIndex', true);
 // 连接数据库
 mongoose.connect(`mongodb://${config.get('db.user')}:${config.get('db.pwd')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}`, {useNewUrlParser: true })
 	.then(() => console.log('数据库连接成功'))
